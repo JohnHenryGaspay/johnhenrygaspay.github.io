80740e1c3d288ec8d3a960be9016cbe235ae168f
John Henry Gaspay <jhgaspay@gmail.com>
2023-08-03 11:56:08 +0800

    add faq page and testimonial on nav menu

--- js/main.js
+++ js/main.js
@@ -32,3 +32,19 @@ $(document).ready(function(){
     });
   });
 })
+// script for faq
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+  
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+  
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
---
+++ sendmail.php
@@ -0,0 +1,23 @@
+<?php
+if (isset($_POST['submit']))
+{
+    $to = "jhgaspay@gmail.com"; // Your email address
+    $name = $_POST['name'];
+    $from = $_POST['email'];
+    $message = $_POST['message'];
+    $headers = "From:" . $from;
+    $result = mail($to, $subject, $message, $headers);
+
+    if ($result)
+    {
+        echo '<script type="text/javascript">alert("Your message was sent!");</script>';
+        echo '<script type="text/javascript">window.location.href = window.location.href;</script>';
+
+    }
+    else
+    {
+        echo '<script type="text/javascript">alert("Oops! Your message wasn’t sent. Try again later.");</script>';
+        echo '<script type="text/javascript">window.location.href = window.location.href;</script>';
+    }
+}
+?>
